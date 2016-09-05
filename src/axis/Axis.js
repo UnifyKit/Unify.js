6112713c99a746cee9c83782e2f91e72ac992b08
UnifyKit <unifykit@gmail.com>
2016-09-05 23:39:04 +0800

    Add autoRotate

--- src/axis/Axis.js
+++ src/axis/Axis.js
@@ -1,3 +1,10 @@
 U.Axis = U.Class.extend({
 
+    init: function(coord) {
+        this.baseCoord = coord;
+        this.autoGridCount = true;
+        this.autoRotate = false;
+        this.rotateAngle = 0;
+
+    }
 });
