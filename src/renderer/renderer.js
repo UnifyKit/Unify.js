0a49ceb8ff9ac040e35e32444956c2cb6f9ccaa8
unifykit@gmail.com <unifykit@gmail.com>
2016-10-26 16:07:12 +0800

    Add Renderer

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,5 +1,20 @@
 U.Renderer = U.Class.extend({
-    init:function (containerId, options ) {
+    init: function (containerId, options) {
         this.container = U.getElement(containerId);
+
+        this.size = options.size !== undefined ? options.size : new U.Size(500, 390);
+        this.resolution = options.resolution !== undefined ? options.resolution : 96;
+    },
+    
+    destroy:function () {
+        this.container = null;
+        this.size =  null;
+        this.resolution = null;
+    },
+
+    supported:function () {
+        return false;
     }
+
+
 })
